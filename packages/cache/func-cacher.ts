5a73e0341339bd7136da2ff179ebc05c641a20b5
guolu <1448896454@qq.com>
2020-12-13 11:47:45 +0800

    func-cacher

---
+++ packages/cache/func-cacher.ts
@@ -0,0 +1,87 @@
+
+type Func = (...args: any[]) => any;
+
+export interface FuncCacherImpl {
+  storage: Map<string, any>;
+  makeCache: (target: object, prop: PropertyKey) => boolean;
+  makeClear: (target: object, prop: PropertyKey) => boolean;
+  unmake: (target: object, prop: PropertyKey) => boolean;
+}
+
+interface FuncCacherOptions {
+  storage?: Map<string, any>;
+}
+
+const NOOP = () => { /** noop */ };
+const cacherFuncList: Func[] = [];
+
+function redefineDescriptor(
+  target: object,
+  prop: PropertyKey,
+  callback: (descriptor: PropertyDescriptor) => PropertyDescriptor | undefined
+): boolean {
+  const descriptor = Reflect.getOwnPropertyDescriptor(target, prop);
+  if (descriptor) {
+    if (typeof descriptor.value === "function") {
+      const newDescriptor = callback(descriptor);
+      if (newDescriptor) {
+        return Reflect.defineProperty(target, prop, newDescriptor);
+      }
+    } else {
+      console.warn(`${prop.toString()} is not a function`)
+    }
+  } else {
+    console.warn(`${prop.toString()} descriptor is undefined`);
+  }
+  return false;
+}
+
+export function createFuncCacher(options: FuncCacherOptions): FuncCacherImpl {
+  const storage = options.storage || new Map();
+  return {
+    storage: storage,
+    makeCache(target, prop) {
+      return redefineDescriptor(target, prop, (descriptor) => {
+        cacherFuncList.push(descriptor.value);
+        const id = cacherFuncList.length - 1;
+        const areAsynceFunc = String(descriptor.value).startsWith("async ");
+        descriptor.value = (...args: any[]) => {
+          let key = id + "#" + JSON.stringify(args);
+          if (storage.has(key)) {
+            return storage.get(key);
+          }
+          const result = Reflect.apply(cacherFuncList[id], target, args);
+          if (areAsynceFunc || (result && result.then)) {
+            return (result as Promise<any>).then((result: any) => {
+              storage.set(key, result);
+              return result;
+            });
+          }
+          return result;
+        }
+        return descriptor;
+      });
+    },
+    makeClear(target, prop,) {
+      return redefineDescriptor(target, prop, (descriptor) => {
+        cacherFuncList.push(descriptor.value);
+        const id = cacherFuncList.length - 1;
+        descriptor.value = (...args: any[]) => {
+          storage.clear();
+          return Reflect.apply(cacherFuncList[id], target, args);
+        }
+        return descriptor;
+      });
+    },
+    unmake(target, prop) {
+      return redefineDescriptor(target, prop, (descriptor) => {
+        const index = cacherFuncList.findIndex(descriptor.value);
+        if (index !== -1) {
+          descriptor.value = cacherFuncList[index];
+          cacherFuncList[index] = NOOP;
+          return descriptor;
+        }
+      });
+    }
+  }
+}
